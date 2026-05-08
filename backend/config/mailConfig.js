45e62b1ee4da28897045ce56c9e95683fe291621
prem07n <premnale66@gmail.com>
2026-05-09 00:39:59 +0530

    fixed nodemailer smtp port

--- backend/config/mailConfig.js
+++ backend/config/mailConfig.js
@@ -1,14 +1,18 @@
 const nodemailer = require("nodemailer");
 
 const transporter = nodemailer.createTransport({
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false,
 
-    service: "gmail",
+  auth: {
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS,
+  },
 
-    auth: {
-        user: process.env.EMAIL_USER,
-        pass: process.env.EMAIL_PASS,
-    },
-
+  tls: {
+    rejectUnauthorized: false,
+  },
 });
 
 module.exports = transporter;
